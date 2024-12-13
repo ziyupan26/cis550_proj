065dc9061cdfdd25a093f5267ada97349f4ef93c
houjipan <131413780+houjipan@users.noreply.github.com>
2024-12-12 23:11:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { amber, lightGreen } from '@mui/material/colors'
 import { createTheme } from "@mui/material/styles";
 
-// import NavBar from './components/NavBar';
-// import HomePage from './pages/HomePage';
-// import AlbumsPage from './pages/AlbumsPage';
-// import SongsPage from './pages/SongsPage';
-// import AlbumInfoPage from './pages/AlbumInfoPage'
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import IngredientPage from './pages/IngredientPage.js';
+import  CategoriesPage from './pages/CategoriesPage';
+import CalculatorPage from './pages/CalculatorPage.js'
 
 // createTheme enables you to customize the look and feel of your app past the default
 // in this case, we only change the color scheme
@@ -29,10 +29,10 @@ export default function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          {/* <Route path="/" element={<HomePage />} />
-          <Route path="/albums" element={<AlbumsPage />} />
-          <Route path="/albums/:album_id" element={<AlbumInfoPage />} />
-          <Route path="/songs" element={<SongsPage />} /> */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/category" element={<CategoriesPage />} />
+          <Route path="/ingredient" element={<IngredientPage />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
